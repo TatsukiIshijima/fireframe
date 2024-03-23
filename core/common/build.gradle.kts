039ae6f31c92caa67b15bcb5bd065a4448fbc3bf
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-23 22:03:59 +0900

    create common module

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.fireframe.android.library)
+    alias(libs.plugins.fireframe.android.hilt)
+}
+
+android {
+    namespace = "com.tatsuki.fireframe.core.common"
+}
+
+dependencies {
+
+}
