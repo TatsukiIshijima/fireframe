8b7091c8757b00681b48164ac8314c27483f6106
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-24 10:30:56 +0900

    add dependencies

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -9,4 +9,9 @@ android {
 
 dependencies {
     api(project(":core:common"))
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
 }
