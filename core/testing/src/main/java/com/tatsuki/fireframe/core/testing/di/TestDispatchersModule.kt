3db683d9d5846ced219f8cf6843e09d1cf7099e4
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-27 09:39:07 +0900

    add modules and rule

---
+++ core/testing/src/main/java/com/tatsuki/fireframe/core/testing/di/TestDispatchersModule.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// https://github.com/android/nowinandroid/blob/main/core/testing/src/main/kotlin/com/google/samples/apps/nowinandroid/core/testing/di/TestDispatchersModule.kt
+
+package com.tatsuki.fireframe.core.testing.di
+
+import com.tatsuki.fireframe.core.common.network.Dispatcher
+import com.tatsuki.fireframe.core.common.network.FireframeDispatchers.Default
+import com.tatsuki.fireframe.core.common.network.FireframeDispatchers.IO
+import com.tatsuki.fireframe.core.common.network.di.DispatcherModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DispatcherModule::class],
+)
+internal object TestDispatchersModule {
+    @Provides
+    @Dispatcher(IO)
+    fun providesIODispatcher(testDispatcher: TestDispatcher): CoroutineDispatcher = testDispatcher
+
+    @Provides
+    @Dispatcher(Default)
+    fun providesDefaultDispatcher(
+        testDispatcher: TestDispatcher,
+    ): CoroutineDispatcher = testDispatcher
+}
