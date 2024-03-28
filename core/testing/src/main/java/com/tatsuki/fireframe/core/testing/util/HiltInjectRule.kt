df434f58ab2085911170df5f919756179f82a5e1
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-28 09:41:31 +0900

    check spotless

--- core/testing/src/main/java/com/tatsuki/fireframe/core/testing/util/HiltInjectRule.kt
+++ core/testing/src/main/java/com/tatsuki/fireframe/core/testing/util/HiltInjectRule.kt
@@ -4,7 +4,7 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.rules.TestWatcher
 
 internal class HiltInjectRule(
-    private val rule: HiltAndroidRule
+    private val rule: HiltAndroidRule,
 ) : TestWatcher() {
     override fun starting(description: org.junit.runner.Description?) {
         super.starting(description)
