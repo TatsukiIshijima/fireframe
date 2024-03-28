09461cd596dea63f45d2ee5a485a673cfcf046fc
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-28 09:40:52 +0900

    change thread

--- core/network/src/main/java/com/tatsuki/fireframe/core/network/di/FakeNetworkModule.kt
+++ core/network/src/main/java/com/tatsuki/fireframe/core/network/di/FakeNetworkModule.kt
@@ -1,6 +1,8 @@
 package com.tatsuki.fireframe.core.network.di
 
 import android.content.Context
+import com.tatsuki.fireframe.core.common.network.Dispatcher
+import com.tatsuki.fireframe.core.common.network.FireframeDispatchers.IO
 import com.tatsuki.fireframe.core.network.OpenWeatherApi
 import com.tatsuki.fireframe.core.network.fake.FakeAssetManager
 import com.tatsuki.fireframe.core.network.fake.FakeOpenWeatherApi
@@ -10,6 +12,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
@@ -36,10 +39,12 @@ internal object FakeOpenWeatherApiModule {
     @Provides
     @Singleton
     fun openWeatherApi(
+        @Dispatcher(IO) ioDispatcher: CoroutineDispatcher,
         networkJson: Json,
         assets: FakeAssetManager,
     ): OpenWeatherApi {
         return FakeOpenWeatherApi(
+            ioDispatcher = ioDispatcher,
             networkJson = networkJson,
             assets = assets,
         )
