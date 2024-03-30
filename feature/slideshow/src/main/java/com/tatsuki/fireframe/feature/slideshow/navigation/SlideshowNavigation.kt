93f69beb85ab1c7319ad78bc6d3dd8dfbd46b6be
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-30 21:53:25 +0900

    create navigation

---
+++ feature/slideshow/src/main/java/com/tatsuki/fireframe/feature/slideshow/navigation/SlideshowNavigation.kt
@@ -0,0 +1,19 @@
+package com.tatsuki.fireframe.feature.slideshow.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.tatsuki.fireframe.feature.slideshow.ui.SlideshowRoute
+
+const val SLIDESHOW_ROUTE = "slide_show_route"
+
+fun NavController.navigateToSlideshow(navOptions: NavOptions? = null) {
+    navigate(SLIDESHOW_ROUTE, navOptions)
+}
+
+fun NavGraphBuilder.slideshowScreen() {
+    composable(route = SLIDESHOW_ROUTE) {
+        SlideshowRoute()
+    }
+}
