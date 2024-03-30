a364e88eb15473a6221fd36ac9d1f09938dcc303
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-30 21:52:05 +0900

    add dependency

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,6 +1,9 @@
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidFeatureConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
@@ -8,6 +11,12 @@ class AndroidFeatureConventionPlugin: Plugin<Project> {
             pluginManager.apply {
                 apply("fireframe.android.library")
                 apply("fireframe.android.hilt")
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+            dependencies {
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
             }
         }
     }
