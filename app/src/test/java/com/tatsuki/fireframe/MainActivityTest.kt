71dac7c32b64613521ab1e66a3813d1eb2d73957
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-31 15:12:18 +0900

    rename

--- app/src/test/java/com/tatsuki/fireframe/MainActivityTest.kt
+++ app/src/test/java/com/tatsuki/fireframe/MainActivityTest.kt
@@ -1,21 +1,16 @@
 package com.tatsuki.fireframe
 
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onRoot
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.takahirom.roborazzi.captureRoboImage
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.HiltTestApplication
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 import javax.inject.Inject
 
@@ -28,23 +23,10 @@ class MainActivityTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
-    fun captureRoboImageSample() {
+    fun capture_robo_image_sample() {
         ActivityScenario.launch(MainActivity::class.java)
 
         onView(ViewMatchers.isRoot())
-            .captureRoboImage()
-    }
-
-    @Test
-    fun sampleTest() {
-        composeTestRule.setContent {
-            MaterialTheme {
-                // Add your Composable here
-                Text("Hello, World!")
-            }
-        }
-        composeTestRule
-            .onRoot()
             .captureRoboImage()
     }
 }
