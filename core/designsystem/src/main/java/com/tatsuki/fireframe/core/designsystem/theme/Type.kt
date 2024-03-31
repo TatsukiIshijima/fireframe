6012746885aef0c7761c6158c2183fe0dda52ea3
TatsukiIshijima <tatsuki445@gmail.com>
2024-03-31 20:57:48 +0900

    move

--- core/designsystem/src/main/java/com/tatsuki/fireframe/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/tatsuki/fireframe/core/designsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.tatsuki.fireframe.ui.theme
+package com.tatsuki.fireframe.core.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
