ec6781847cb3b9f386283fa6f28736f8fa3d30c7
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-07 10:54:29 +0900

    create data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.fireframe.android.library)
+    alias(libs.plugins.fireframe.android.hilt)
+}
+
+android {
+    namespace = "com.tatsuki.fireframe.core.data"
+}
+
+dependencies {
+    api(project(":core:common"))
+    api(project(":core:network"))
+}
