badcb8c9693205827d1c483d9a433dab487b10ad
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-13 23:26:40 +0900

    create module

---
+++ feature/onboarding/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.fireframe.android.feature)
+    alias(libs.plugins.fireframe.android.library.compose)
+    alias(libs.plugins.roborazzi)
+}
+
+android {
+    namespace = "com.tatsuki.fireframe.feature.onboarding"
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation(project(":core:data"))
+
+    testImplementation(project(":core:testing"))
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.roborazzi)
+
+    androidTestImplementation(project(":core:testing"))
+}
