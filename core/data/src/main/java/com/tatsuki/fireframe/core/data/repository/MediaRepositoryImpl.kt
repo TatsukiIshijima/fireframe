faa7c91c8bab737f4c2919cea789868f6e4272d0
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-16 00:00:34 +0900

    create repository

---
+++ core/data/src/main/java/com/tatsuki/fireframe/core/data/repository/MediaRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.tatsuki.fireframe.core.data.repository
+
+import com.tatsuki.fireframe.core.database.MediaDataClient
+import com.tatsuki.fireframe.core.model.MediaImage
+import com.tatsuki.fireframe.core.model.MediaImageDirectory
+import javax.inject.Inject
+
+class MediaRepositoryImpl @Inject constructor(
+    private val mediaDataClient: MediaDataClient,
+) : MediaRepository {
+
+    override suspend fun getAllImageDirectories(): List<MediaImageDirectory> {
+        return mediaDataClient.queryAllImageDirectories()
+    }
+
+    override suspend fun getImagesFromDirectory(name: String): List<MediaImage> {
+        return mediaDataClient.queryImagesFromDirectory(name)
+    }
+}
