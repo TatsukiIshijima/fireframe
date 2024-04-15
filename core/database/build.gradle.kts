52004c2d7c310420f520c1311d677265594a5cb3
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-15 22:07:28 +0900

    create database module

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.fireframe.android.library)
+    alias(libs.plugins.fireframe.android.hilt)
+}
+
+android {
+    namespace = "com.tatsuki.fireframe.core.database"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+
+}
