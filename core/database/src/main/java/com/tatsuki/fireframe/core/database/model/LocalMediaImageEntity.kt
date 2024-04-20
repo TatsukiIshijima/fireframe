966441700fa5148dad1d83283adbb5da7165d100
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-20 21:52:22 +0900

    create entity

---
+++ core/database/src/main/java/com/tatsuki/fireframe/core/database/model/LocalMediaImageEntity.kt
@@ -0,0 +1,17 @@
+package com.tatsuki.fireframe.core.database.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.tatsuki.fireframe.core.model.MediaImage
+
+@Entity(
+    tableName = "local_media_image",
+)
+data class LocalMediaImageEntity(
+    @PrimaryKey
+    val id: Long,
+)
+
+fun LocalMediaImageEntity.asExternalModel() = MediaImage(
+    id = id,
+)
