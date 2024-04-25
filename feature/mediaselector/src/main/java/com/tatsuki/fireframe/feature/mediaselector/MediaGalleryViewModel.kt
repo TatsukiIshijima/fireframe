0186a17e1e5dca975544cb7d3ad6d6a52c45b97d
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-25 10:19:06 +0900

    create media gallery screen

---
+++ feature/mediaselector/src/main/java/com/tatsuki/fireframe/feature/mediaselector/MediaGalleryViewModel.kt
@@ -0,0 +1,37 @@
+package com.tatsuki.fireframe.feature.mediaselector
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tatsuki.fireframe.core.data.repository.MediaRepository
+import com.tatsuki.fireframe.feature.mediaselector.model.SelectableMediaImage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MediaGalleryViewModel @Inject constructor(
+    private val mediaRepository: MediaRepository,
+) : ViewModel() {
+
+    private val mutableSelectedImages =
+        MutableStateFlow(emptyList<SelectableMediaImage>())
+    val selectedImages = mutableSelectedImages.asStateFlow()
+
+    fun loadSelectedImages() {
+        viewModelScope.launch {
+            try {
+                val selectedImages = mediaRepository.getSelectedLocalMediaImages()
+                mutableSelectedImages.value = selectedImages.map { image ->
+                    SelectableMediaImage.from(image)
+                }
+            } catch (e: Exception) {
+                Log.d("MediaGalleryViewModel", "Failed to load selected images", e)
+                coroutineContext.ensureActive()
+            }
+        }
+    }
+}
