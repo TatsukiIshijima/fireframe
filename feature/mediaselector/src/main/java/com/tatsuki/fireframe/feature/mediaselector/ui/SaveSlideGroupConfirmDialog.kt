95ddfefa0ed273fd5a7bd71c5548846ff54ead4e
TatsukiIshijima <tatsuki445@gmail.com>
2024-04-30 15:50:21 +0900

    create dialog background

--- feature/mediaselector/src/main/java/com/tatsuki/fireframe/feature/mediaselector/ui/SaveSlideGroupConfirmDialog.kt
+++ feature/mediaselector/src/main/java/com/tatsuki/fireframe/feature/mediaselector/ui/SaveSlideGroupConfirmDialog.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.foundation.text2.input.TextFieldLineLimits
 import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,6 +27,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.tatsuki.fireframe.core.designsystem.component.DialogBackground
 import com.tatsuki.fireframe.feature.mediaselector.R
 
 @Composable
@@ -53,71 +53,70 @@ private fun SaveSlideGroupConfirmDialogContent(
     modifier: Modifier = Modifier,
 ) {
     val textFieldState = rememberTextFieldState()
-    Card(
-        modifier = modifier,
-        shape = RoundedCornerShape(16.dp),
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = stringResource(id = R.string.save_slide_group_confirm_dialog_message),
-                color = MaterialTheme.colorScheme.onSurface,
-                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                fontWeight = MaterialTheme.typography.bodyLarge.fontWeight,
-                textAlign = TextAlign.Center,
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Box(
+    DialogBackground(
+        content = {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = RoundedCornerShape(4.dp),
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = RoundedCornerShape(4.dp),
-                    ),
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                BasicTextField2(
-                    state = textFieldState,
+                Text(
+                    text = stringResource(id = R.string.save_slide_group_confirm_dialog_message),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    fontWeight = MaterialTheme.typography.bodyLarge.fontWeight,
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp),
-                    textStyle = MaterialTheme.typography.titleMedium,
-                    lineLimits = TextFieldLineLimits.SingleLine,
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier,
-            ) {
-                Button(
-                    onClick = onDismissRequest,
-                    modifier = Modifier
-                        .weight(1f),
+                        .background(
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = RoundedCornerShape(4.dp),
+                        )
+                        .border(
+                            width = 1.dp,
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = RoundedCornerShape(4.dp),
+                        ),
                 ) {
-                    Text(text = stringResource(id = R.string.cancel_button))
+                    BasicTextField2(
+                        state = textFieldState,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        textStyle = MaterialTheme.typography.titleMedium,
+                        lineLimits = TextFieldLineLimits.SingleLine,
+                    )
                 }
-                Spacer(modifier = Modifier.widthIn(16.dp))
-                Button(
-                    onClick = {
-                        onSaveRequest(textFieldState.text.toString())
-                        onDismissRequest()
-                    },
-                    modifier = Modifier.weight(1f),
-                    enabled = textFieldState.text.isNotEmpty(),
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier,
                 ) {
-                    Text(text = stringResource(id = R.string.save_button))
+                    Button(
+                        onClick = onDismissRequest,
+                        modifier = Modifier
+                            .weight(1f),
+                    ) {
+                        Text(text = stringResource(id = R.string.cancel_button))
+                    }
+                    Spacer(modifier = Modifier.widthIn(16.dp))
+                    Button(
+                        onClick = {
+                            onSaveRequest(textFieldState.text.toString())
+                            onDismissRequest()
+                        },
+                        modifier = Modifier.weight(1f),
+                        enabled = textFieldState.text.isNotEmpty(),
+                    ) {
+                        Text(text = stringResource(id = R.string.save_button))
+                    }
                 }
             }
-        }
-    }
+        },
+    )
 }
 
 @Preview
