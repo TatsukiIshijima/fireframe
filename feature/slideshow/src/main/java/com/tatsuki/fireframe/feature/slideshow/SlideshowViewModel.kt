f9071068c55b76e0f1a807d13f23cfeaa8888ff8
TatsukiIshijima <tatsuki445@gmail.com>
2024-05-13 09:27:04 +0900

    load local images

--- feature/slideshow/src/main/java/com/tatsuki/fireframe/feature/slideshow/SlideshowViewModel.kt
+++ feature/slideshow/src/main/java/com/tatsuki/fireframe/feature/slideshow/SlideshowViewModel.kt
@@ -1,24 +1,51 @@
 package com.tatsuki.fireframe.feature.slideshow
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tatsuki.fireframe.core.data.repository.MediaRepository
+import com.tatsuki.fireframe.core.data.repository.SettingRepository
 import com.tatsuki.fireframe.core.data.repository.WeatherRepository
 import com.tatsuki.fireframe.core.model.CurrentAndForecastWeather
+import com.tatsuki.fireframe.feature.slideshow.model.SlideImage
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SlideshowViewModel @Inject constructor(
+    private val mediaRepository: MediaRepository,
+    private val settingRepository: SettingRepository,
     private val weatherRepository: WeatherRepository,
 ) : ViewModel() {
+
+    private val mutableSlideImages = MutableStateFlow<List<SlideImage>>(emptyList())
+    val slideImages = mutableSlideImages.asStateFlow()
 
     private val mutableCurrentAndForecastWeather =
         MutableStateFlow<CurrentAndForecastWeather?>(null)
     val currentAndForecastWeather = mutableCurrentAndForecastWeather.asStateFlow()
+
+    fun onCreate() {
+        loadSlideGroup()
+    }
+
+    private fun loadSlideGroup() {
+        viewModelScope.launch {
+            try {
+                val selectedSlideGroupId = settingRepository.selectedSlideGroupIdFlow.first()
+                val slideImages = mediaRepository.getSlideGroupImages(selectedSlideGroupId)
+                    .map { slideGroupImage -> SlideImage.from(slideGroupImage) }
+                mutableSlideImages.value = slideImages
+            } catch (e: Exception) {
+                Log.e("SlideshowViewModel", "Failed to load selected images", e)
+            }
+        }
+    }
 
     fun fetchCurrentAndForecastWeather() {
         viewModelScope.launch {
--- feature/slideshow/src/main/java/com/tatsuki/fireframe/feature/slideshow/ui/SlideshowScreen.kt
+++ feature/slideshow/src/main/java/com/tatsuki/fireframe/feature/slideshow/ui/SlideshowScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,6 +30,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.tatsuki.fireframe.core.designsystem.component.FireframeAsyncImage
+import com.tatsuki.fireframe.core.designsystem.component.Placeholder
 import com.tatsuki.fireframe.core.designsystem.theme.FireframeTheme
 import com.tatsuki.fireframe.core.model.CurrentAndForecastWeather
 import com.tatsuki.fireframe.core.ui.DateText
@@ -37,6 +39,7 @@ import com.tatsuki.fireframe.core.ui.TextClock
 import com.tatsuki.fireframe.core.ui.WeatherIcon
 import com.tatsuki.fireframe.feature.slideshow.R
 import com.tatsuki.fireframe.feature.slideshow.SlideshowViewModel
+import com.tatsuki.fireframe.feature.slideshow.model.SlideImage
 
 @Composable
 internal fun SlideshowRoute(
@@ -44,28 +47,19 @@ internal fun SlideshowRoute(
     modifier: Modifier = Modifier,
     slideshowViewModel: SlideshowViewModel = hiltViewModel(),
 ) {
-    val photoUrls = listOf(
-        "https://pelipecky.sk/wp-content/uploads/2020/03/cesta-domov-400x240.jpg",
-        "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
-        "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FRB_486265257EDR_F0481570FHAZ00323M_.JPG",
-        "https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RLB_486265291EDR_F0481570RHAZ00323M_.JPG",
-        "https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RRB_486265291EDR_F0481570RHAZ00323M_.JPG",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631300305227E03_DXXX.jpg",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631290503689E01_DXXX.jpg",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631290305226E03_DXXX.jpg",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631280503688E0B_DXXX.jpg",
-        "https://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631280305225E03_DXXX.jpg",
-    )
+    val slideImages by slideshowViewModel.slideImages.collectAsStateWithLifecycle()
     val currentAndForecastWeather by slideshowViewModel.currentAndForecastWeather.collectAsStateWithLifecycle()
 
     SlideshowScreen(
         isEnableWeather = isEnableWeather,
-        batteryLevel = 50,
-        photoUrls = photoUrls,
+        slideImages = slideImages,
         currentAndForecastWeather = currentAndForecastWeather,
         modifier = modifier,
     )
+
+    LaunchedEffect(Unit) {
+        slideshowViewModel.onCreate()
+    }
 
     // FIXME: prevent call twice
     if (isEnableWeather) {
@@ -80,43 +74,39 @@ internal fun SlideshowRoute(
 @Composable
 internal fun SlideshowScreen(
     isEnableWeather: Boolean,
-    batteryLevel: Int,
-    photoUrls: List<Any?>,
+    slideImages: List<SlideImage>,
     currentAndForecastWeather: CurrentAndForecastWeather?,
     modifier: Modifier = Modifier,
 ) {
+    val isLocalInspection = LocalInspectionMode.current
     Box(
         modifier = modifier
             .fillMaxSize(),
     ) {
-        if (photoUrls.isEmpty()) {
+        if (slideImages.isEmpty()) {
             // TODO: Add error handling
         } else {
             HorizontalAutoLoopPager(
-                pageCount = photoUrls.size,
+                pageCount = slideImages.size,
                 modifier = Modifier.fillMaxSize(),
                 beyondBoundsPageCount = 1,
                 delayMills = 5000,
             ) { page ->
-                FireframeAsyncImage(
-                    model = photoUrls[page],
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop,
-                )
+                if (isLocalInspection) {
+                    Placeholder(
+                        modifier = Modifier.fillMaxSize(),
+                        text = "Image$page",
+                    )
+                } else {
+                    FireframeAsyncImage(
+                        model = slideImages[page].uri,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
             }
         }
-//        Box(
-//            modifier = Modifier
-//                .align(Alignment.TopEnd),
-//        ) {
-//            BatteryIcon(
-//                level = batteryLevel,
-//                modifier = Modifier
-//                    .size(58.dp)
-//                    .padding(16.dp),
-//            )
-//        }
         Box(
             modifier = Modifier
                 .align(Alignment.BottomStart),
@@ -200,9 +190,8 @@ fun SlideshowScreenTabletPreview() {
     FireframeTheme {
         SlideshowScreen(
             isEnableWeather = true,
-            batteryLevel = 50,
-            photoUrls = listOf(
-                R.drawable.dummy_image,
+            slideImages = listOf(
+                SlideImage.fake(),
             ),
             currentAndForecastWeather = CurrentAndForecastWeather.fake(),
         )
@@ -218,9 +207,8 @@ fun SlideshowScreenMobilePreview() {
     FireframeTheme {
         SlideshowScreen(
             isEnableWeather = true,
-            batteryLevel = 50,
-            photoUrls = listOf(
-                R.drawable.dummy_image,
+            slideImages = listOf(
+                SlideImage.fake(),
             ),
             currentAndForecastWeather = CurrentAndForecastWeather.fake(),
         )
