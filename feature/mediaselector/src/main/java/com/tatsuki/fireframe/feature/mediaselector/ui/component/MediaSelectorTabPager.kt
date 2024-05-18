35449e0a34983eb9d31e03ef6d38e2bda2db3c93
TatsukiIshijima <tatsuki445@gmail.com>
2024-05-18 17:43:11 +0900

    change to scrollable

--- feature/mediaselector/src/main/java/com/tatsuki/fireframe/feature/mediaselector/ui/component/MediaSelectorTabPager.kt
+++ feature/mediaselector/src/main/java/com/tatsuki/fireframe/feature/mediaselector/ui/component/MediaSelectorTabPager.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerScope
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -30,6 +32,9 @@ internal fun MediaSelectorTabPager(
             tabNames = tabNames,
             pagerState = pagerState,
             modifier = Modifier.fillMaxWidth(),
+        )
+        HorizontalDivider(
+            thickness = 1.dp
         )
         HorizontalPager(
             state = pagerState,
