8cc5431517b5bf7589dcfc3244292e556f925789
TatsukiIshijima <tatsuki445@gmail.com>
2024-05-18 15:16:53 +0900

    add test

--- feature/home/src/test/java/com/tatsuki/fireframe/feature/home/HomeScreenshotTest.kt
+++ feature/home/src/test/java/com/tatsuki/fireframe/feature/home/HomeScreenshotTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onRoot
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.takahirom.roborazzi.captureRoboImage
+import com.tatsuki.fireframe.core.model.SlideGroup
 import com.tatsuki.fireframe.feature.home.model.HomeState
 import com.tatsuki.fireframe.feature.home.ui.HomeScreen
 import org.junit.Rule
@@ -24,6 +25,41 @@ class HomeScreenshotTest {
         composeTestRule.setContent {
             HomeScreen(
                 homeState = HomeState.fake(),
+            )
+        }
+
+        composeTestRule
+            .onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    fun capture_home_screen_disable_start_button_when_slide_group_empty() {
+        composeTestRule.setContent {
+            HomeScreen(
+                homeState = HomeState.fake(
+                    slideGroups = emptyList(),
+                ),
+            )
+        }
+
+        composeTestRule
+            .onRoot()
+            .captureRoboImage()
+    }
+
+    @Test
+    fun capture_home_screen_disable_source_buttons_when_slide_group_max() {
+        composeTestRule.setContent {
+            HomeScreen(
+                homeState = HomeState.fake(
+                    slideGroups = (0..10).map {
+                        SlideGroup.fake(
+                            id = it.toLong(),
+                            groupName = "SlideGroup$it",
+                        )
+                    },
+                ),
             )
         }
 
