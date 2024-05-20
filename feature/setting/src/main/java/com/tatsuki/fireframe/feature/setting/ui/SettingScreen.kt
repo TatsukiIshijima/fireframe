c01d31c4e65161855842e3b58e7dd169d058b5c3
TatsukiIshijima <tatsuki445@gmail.com>
2024-05-21 08:32:56 +0900

    refactor

--- feature/setting/src/main/java/com/tatsuki/fireframe/feature/setting/ui/SettingScreen.kt
+++ feature/setting/src/main/java/com/tatsuki/fireframe/feature/setting/ui/SettingScreen.kt
@@ -1,14 +1,23 @@
 package com.tatsuki.fireframe.feature.setting.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tatsuki.fireframe.core.designsystem.component.TopAppBar
 import com.tatsuki.fireframe.feature.setting.R
 
@@ -39,6 +48,61 @@ internal fun SettingScreen(
             navigationIcon = Icons.AutoMirrored.Filled.ArrowBack,
             navigationIconDescription = "Back",
             onNavigationClick = onBack,
+        )
+        SettingItem(
+            title = stringResource(id = R.string.slideshow_interval),
+            value = "1分",
+            onClick = {
+                // TODO: Implement
+            },
+        )
+        SettingItem(
+            title = stringResource(id = R.string.content_scale_type),
+            value = "中央",
+            onClick = {
+                // TODO: Implement
+            },
+        )
+        SettingItem(
+            title = stringResource(id = R.string.app_version),
+            value = "1.0.2",
+        )
+    }
+}
+
+@Composable
+private fun SettingItem(
+    title: String,
+    value: String,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+) {
+    val clickableModifier = if (onClick != null) {
+        modifier
+            .fillMaxWidth()
+            .clickable {
+                onClick()
+            }
+            .padding(16.dp)
+    } else {
+        modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    }
+    Row(
+        modifier = clickableModifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = title,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+        Text(
+            text = value,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyMedium,
         )
     }
 }
