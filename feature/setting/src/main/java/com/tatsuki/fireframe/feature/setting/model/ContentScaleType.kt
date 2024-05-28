c355b35b842177b95c2357cc2f637f72e362e72d
TatsukiIshijima <tatsuki445@gmail.com>
2024-05-28 09:54:10 +0900

    fix

--- feature/setting/src/main/java/com/tatsuki/fireframe/feature/setting/model/ContentScaleType.kt
+++ feature/setting/src/main/java/com/tatsuki/fireframe/feature/setting/model/ContentScaleType.kt
@@ -1,6 +1,12 @@
 package com.tatsuki.fireframe.feature.setting.model
 
 import com.tatsuki.fireframe.core.designsystem.model.SelectableItem
+import com.tatsuki.fireframe.core.model.ContentScaleType.CROP
+import com.tatsuki.fireframe.core.model.ContentScaleType.FILL_HEIGHT
+import com.tatsuki.fireframe.core.model.ContentScaleType.FILL_WIDTH
+import com.tatsuki.fireframe.core.model.ContentScaleType.FIT
+import com.tatsuki.fireframe.core.model.ContentScaleType.INSIDE
+import com.tatsuki.fireframe.core.model.ContentScaleType as ScaleType
 
 sealed interface ContentScaleType : SelectableItem {
 
@@ -24,15 +30,31 @@ sealed interface ContentScaleType : SelectableItem {
         override val name: String = "インサイド",
     ) : ContentScaleType
 
+    fun toDomain(): ScaleType {
+        return when (this) {
+            is Crop -> CROP
+            is Fit -> FIT
+            is FillHeight -> FILL_HEIGHT
+            is FillWidth -> FILL_WIDTH
+            is Inside -> INSIDE
+        }
+    }
+
     companion object {
+
+        fun from(scaleType: ScaleType): ContentScaleType {
+            return when (scaleType) {
+                CROP -> Crop()
+                FIT -> Fit()
+                FILL_HEIGHT -> FillHeight()
+                FILL_WIDTH -> FillWidth()
+                INSIDE -> Inside()
+            }
+        }
+
         fun all(): List<ContentScaleType> {
-            return listOf(
-                Crop(),
-                Fit(),
-                FillHeight(),
-                FillWidth(),
-                Inside(),
-            )
+            return ScaleType.entries
+                .map { from(it) }
         }
     }
 }
